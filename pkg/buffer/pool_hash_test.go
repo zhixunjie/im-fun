7c15f6bc899d7c7322d0077609f60918940e29a6
JasonZhi <369610501@qq.com>
2023-02-28 02:39:38 +0800

    bufio

--- pkg/buffer/pool_hash_test.go
+++ pkg/buffer/pool_hash_test.go
@@ -24,16 +24,18 @@ func TestPoolHash(t *testing.T) {
 	pool := poolHash.Reader(101)
 	buf := pool.Get()
 	byteArr := buf.Bytes()
-	fmt.Printf("%s\n", byteArr)
+	// fmt.Printf("%s\n", byteArr)
 
 	// Bufio
 	bufioReader := bufio.Reader{}
+	// connReader := bytes.NewReader([]byte("abc\ndef\n")) // suppose this is a connection fd
 	connReader := bytes.NewReader([]byte("abc\ndef")) // suppose this is a connection fd
 	bufioReader.ResetBuffer(connReader, byteArr)
 
 	// Read
+	// TODO 当connReader中的字符串最后没有换行，下面的输出会有bug，这个应该属于bufio的bug
 	line1, _, _ := bufioReader.ReadLine()
-	fmt.Printf("%s\n", line1)
 	line2, _, _ := bufioReader.ReadLine()
+	fmt.Printf("%s\n", line1)
 	fmt.Printf("%s\n", line2)
 }
