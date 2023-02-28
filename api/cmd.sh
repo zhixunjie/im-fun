12cbff39a2348b7717659a87da77f542ae2f4ff4
JasonZhi <369610501@qq.com>
2023-02-28 11:48:05 +0800

    proto

--- api/cmd.sh
+++ api/cmd.sh
@@ -1,3 +1,14 @@
-protoc --go-grpc_out=. --go-grpc_opt=paths=source_relative protocol/protocol.proto
-protoc --go-grpc_out=. --go-grpc_opt=paths=source_relative logic/logic.proto
-protoc --go-grpc_out=. --go-grpc_opt=paths=source_relative comet/comet.proto
+# protocol
+protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    protocol/protocol.proto
+
+# logic
+protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    logic/logic.proto
+
+# comet
+protoc --go_out=. --go_opt=paths=source_relative \
+    --go-grpc_out=. --go-grpc_opt=paths=source_relative \
+    comet/comet.proto
