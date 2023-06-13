9856aaa6623c47c06914af8491fe9f6a5e6f6648
JasonZhi <369610501@qq.com>
2023-06-14 05:58:01 +0800

    feat: 更换log

--- pkg/logging/caller.go
+++ pkg/logging/caller.go
@@ -18,9 +18,20 @@ func PrintCaller(f *runtime.Frame) (string, string) {
 
 // PrintCallerOther 输出文件
 func PrintCallerOther(f *runtime.Frame) string {
-	s := strings.Split(f.Function, ".")
-	funcName := s[len(s)-1]
-	dir, filename := path.Split(f.File)
+	pc, file, line, _ := runtime.Caller(9)
+	dir, filename := path.Split(file)
 	baseDir := path.Base(dir)
-	return fmt.Sprintf("%v/%v:%v:[%v]", baseDir, filename, f.Line, funcName)
+	// get function info
+	sFun := runtime.FuncForPC(pc)
+	s := strings.Split(sFun.Name(), ".")
+	funcName := s[len(s)-1]
+
+	// 原来的机制：
+	//s := strings.Split(f.Function, ".")
+	//funcName := s[len(s)-1]
+	//dir, filename := path.Split(f.File)
+	//baseDir := path.Base(dir)
+	//return fmt.Sprintf("%v/%v:%v:[%v]", baseDir, filename, f.Line, funcName)
+
+	return fmt.Sprintf("%v/%v:%v:[%v]", baseDir, filename, line, funcName)
 }
