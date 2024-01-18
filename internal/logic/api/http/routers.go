fd18250e71ce163c086d934aeafca6850f46ca8d
JasonZhi <369610501@qq.com>
2024-01-19 04:36:53 +0800

    feat: 增加日志

--- internal/logic/api/http/routers.go
+++ internal/logic/api/http/routers.go
@@ -9,22 +9,28 @@ func (s *Server) SetupRouter() {
 	// message
 	g1 := router.Group("/message")
 	{
-		g1.POST("/send", s.sendMessage)
-		g1.POST("/fetch", s.fetchMessage)
+		g1.POST("/send", s.sendMessage)           // 发送消息（普通消息） TODO 结合缓存机制优化
+		g1.POST("/send/system", s.sendMessage)    // TODO 发送消息（系统消息）
+		g1.POST("/fetch", s.fetchMessage)         // version_id拉取：消息列表 TODO 结合缓存机制优化
+		g1.POST("/clean", s.fetchMessage)         // TODO 清空聊天记录
+		g1.POST("/has/read", s.fetchMessage)      // TODO 消息已读
+		g1.POST("/update/status", s.fetchMessage) // TODO 修改消息状态：消息删除 & 撤回消息
 	}
 
 	// contact
 	g2 := router.Group("/contact")
 	{
-		g2.POST("/fetch", s.fetchContact)
+		g2.POST("/fetch", s.fetchContact)  // version_id拉取：会话列表 TODO 结合缓存机制优化
+		g1.POST("/delete", s.fetchContact) // TODO 删除一个会话
+		g1.POST("/stick", s.fetchContact)  // TODO 会话置顶
 	}
 
 	// push
 	group := s.engine.Group("/im")
 	{
-		group.POST("/send/user/keys", s.sendToUserKeys) // 发送给指定的用户key
-		group.POST("/send/user/ids", s.sendToUserIds)   // 发送给指定的用户id
-		group.POST("/send/user/room", s.sendToRoom)     // 广播给房间的所有用户
-		group.POST("/send/user/all", s.sendToAll)       // 广播给所有用户
+		group.POST("/send/user/keys", s.sendToUserKeys) // 发送：给指定的用户key
+		group.POST("/send/user/ids", s.sendToUserIds)   // 发送：给指定的用户id
+		group.POST("/send/user/room", s.sendToRoom)     // 广播：给房间的所有用户
+		group.POST("/send/user/all", s.sendToAll)       // 广播：给所有用户
 	}
 }
