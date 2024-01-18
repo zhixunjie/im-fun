2a0eb727cb90b3488626acf304370a2f71046e23
JasonZhi <369610501@qq.com>
2024-01-19 04:14:30 +0800

    feat: fetch contact

--- internal/logic/api/http/http_contact.go
+++ internal/logic/api/http/http_contact.go
@@ -1,1 +1,29 @@
 package http
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/zhixunjie/im-fun/internal/logic/data/ent/request"
+	"github.com/zhixunjie/im-fun/internal/logic/data/ent/response"
+	"net/http"
+)
+
+func (s *Server) fetchContact(ctx *gin.Context) {
+	// request
+	var req request.FetchContactReq
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		response.JsonError(ctx, err)
+		return
+	}
+
+	// biz
+	resp, err := s.BzContact.FetchContact(ctx, &req)
+
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"msg": "error: " + err.Error()})
+		return
+	}
+
+	// resp
+	ctx.JSON(http.StatusOK, resp)
+	return
+}
--- internal/logic/api/http/http_message.go
+++ internal/logic/api/http/http_message.go
@@ -53,24 +53,3 @@ func (s *Server) fetchMessage(ctx *gin.Context) {
 	ctx.JSON(http.StatusOK, resp)
 	return
 }
-
-func (s *Server) fetchContact(ctx *gin.Context) {
-	// request
-	var req request.FetchContactReq
-	if err := ctx.ShouldBindJSON(&req); err != nil {
-		response.JsonError(ctx, err)
-		return
-	}
-
-	// biz
-	resp, err := s.BzContact.FetchContact(ctx, &req)
-
-	if err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"msg": "error: " + err.Error()})
-		return
-	}
-
-	// resp
-	ctx.JSON(http.StatusOK, resp)
-	return
-}
