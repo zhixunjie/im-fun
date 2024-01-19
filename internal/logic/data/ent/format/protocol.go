06da4176a74e07f0bfa00f1827cc7c1197b21d6d
JasonZhi <369610501@qq.com>
2024-01-19 15:32:06 +0800

    feat: docs

--- internal/logic/data/ent/format/protocol.go
+++ internal/logic/data/ent/format/protocol.go
@@ -9,11 +9,11 @@ type MsgBody struct {
 	MsgContent *MsgContent `json:"msg_content"`
 }
 
+// MsgContent 不同消息类型，对应不同的结构体
 type MsgContent struct {
-	// 不同消息类型对应不同的结构体
+	CustomContent *CustomContent `json:"custom_content,omitempty"` // 自定义消息
 	TextContent   *TextContent   `json:"text_content,omitempty"`   // 文本消息
 	TipsContent   *TipsContent   `json:"tips_content,omitempty"`   // 提示消息
-	CustomContent *CustomContent `json:"custom_content,omitempty"` // 自定义消息
 	ImageContent  *ImageContent  `json:"image_content,omitempty"`  // 图片消息
 	AudioContent  *AudioContent  `json:"audio_content,omitempty"`  // 音频消息
 
