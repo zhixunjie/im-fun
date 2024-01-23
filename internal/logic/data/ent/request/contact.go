b6fb7ef82d7a1639dfea9423dce2b226a3486621
JasonZhi <369610501@qq.com>
2024-01-23 11:43:24 +0800

    feat: docs

---
+++ internal/logic/data/ent/request/contact.go
@@ -0,0 +1,10 @@
+package request
+
+import "github.com/zhixunjie/im-fun/internal/logic/data/ent/generate/model"
+
+// ContactFetchReq 拉取会话列表（by version_id）
+type ContactFetchReq struct {
+	VersionId model.BigIntType    `json:"version_id"` // 版本id
+	OwnerId   model.BigIntType    `json:"owner_id"`   // 会话拥有者
+	OwnerType model.ContactIdType `json:"owner_type"` // 会话拥有者的用户类型
+}
--- internal/logic/data/ent/request/request.go
+++ internal/logic/data/ent/request/request.go
@@ -1,38 +1,5 @@
 package request
 
-import (
-	"github.com/zhixunjie/im-fun/internal/logic/data/ent/format"
-	"github.com/zhixunjie/im-fun/internal/logic/data/ent/generate/model"
-)
-
 type PingReq struct {
 	Id int `json:"id"`
-}
-
-// MessageSendReq 发送消息给某个用户
-type MessageSendReq struct {
-	SeqId         model.BigIntType    `json:"seq_id"`         // 消息唯一id（客户端）
-	SenderId      model.BigIntType    `json:"sender_id"`      // 消息发送者id
-	SenderType    model.ContactIdType `json:"sender_type"`    // 消息发送者的用户类型
-	ReceiverId    model.BigIntType    `json:"receiver_id"`    // 消息接收者id
-	ReceiverType  model.ContactIdType `json:"receiver_type"`  // 消息接收者的用户类型
-	MsgBody       format.MsgBody      `json:"msg_body"`       // 消息体
-	InvisibleList []uint64            `json:"invisible_list"` // 不可见的列表
-}
-
-// MessageFetchReq 拉取消息列表（by version_id）
-type MessageFetchReq struct {
-	FetchType model.FetchType     `json:"fetch_type"`
-	VersionId model.BigIntType    `json:"version_id"` // 版本id
-	OwnerId   model.BigIntType    `json:"owner_id"`   // 会话拥有者
-	OwnerType model.ContactIdType `json:"owner_type"` // 会话拥有者的用户类型
-	PeerId    model.BigIntType    `json:"peer_id"`    // 会话联系人（对方）
-	PeerType  model.ContactIdType `json:"peer_type"`  // 会话联系人（对方）的用户类型
-}
-
-// ContactFetchReq 拉取会话列表（by version_id）
-type ContactFetchReq struct {
-	VersionId model.BigIntType    `json:"version_id"` // 版本id
-	OwnerId   model.BigIntType    `json:"owner_id"`   // 会话拥有者
-	OwnerType model.ContactIdType `json:"owner_type"` // 会话拥有者的用户类型
 }
