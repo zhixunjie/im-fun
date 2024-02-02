927042f2014dceabe954c4a75d7bc8e958909b28
JasonZhi <369610501@qq.com>
2024-02-02 16:57:50 +0800

    feat: 自定义消息

--- internal/logic/data/ent/format/custom.go
+++ internal/logic/data/ent/format/custom.go
@@ -3,7 +3,8 @@ package format
 // 自定义消息
 
 type CustomContent struct {
-	Data string `json:"data,omitempty"` // 自定义消息（一般会采用JSON格式）
+	Event string `json:"event"`
+	Data  string `json:"data,omitempty"` // 自定义消息（一般会采用JSON格式）
 }
 
 func (c CustomContent) GetType() MsgType {
@@ -11,4 +12,21 @@ func (c CustomContent) GetType() MsgType {
 }
 
 //......
-// 补充各类自定义的结构体
+// 根据不同的Event，把Data解析为不同的结构体
+// note：解析一条自定义消息，需要两次Unmarshal
+
+const (
+	CustomMsgEventNONE    = iota
+	CustomMsgEventLevelUp // 人物升级（对应结构体：ImJsonLevel）
+	CustomMsgEventDrop    // 物品掉落（对应结构体：ImJsonDrop）
+)
+
+type ImJsonLevel struct {
+	Uid       uint64 // 谁升级了？
+	CurrLevel int    // 当前等级是？
+}
+
+type ImJsonDrop struct {
+	Uid     uint64 // 掉落给谁？
+	GoodsId int    // 物品id
+}
