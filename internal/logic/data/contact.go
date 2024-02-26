d45397e30c500b22fae481bd99a4a83cc1652ced
JasonZhi <369610501@qq.com>
2024-02-26 16:46:48 +0800

    feat: docs

--- internal/logic/data/contact.go
+++ internal/logic/data/contact.go
@@ -164,8 +164,7 @@ func (repo *ContactRepo) UpdateLastMsgId(ctx context.Context, logHead string, la
 	}
 
 	// save to db（要求：数据库的最后一条消息id小于当前消息id）
-	var res gen.ResultInfo
-	res, err = qModel.Where(qModel.ID.Eq(contactId), qModel.LastMsgID.Lt(lastMsgId)).Limit(1).Updates(row)
+	res, err := qModel.Where(qModel.ID.Eq(contactId), qModel.LastMsgID.Lt(lastMsgId)).Limit(1).Updates(row)
 	if err != nil {
 		logging.Errorf(logHead+"Updates fail,err=%v,contact=%v", err, row)
 		return
