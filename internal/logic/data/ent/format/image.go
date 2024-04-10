659acca59a85fcf58977b191f6e08b2ac5d288b5
JasonZhi <369610501@qq.com>
2024-04-10 17:25:45 +0800

    feat: 修正资源类型

--- internal/logic/data/ent/format/image.go
+++ internal/logic/data/ent/format/image.go
@@ -10,6 +10,7 @@ func (c ImageContent) GetType() MsgType {
 }
 
 type Image struct {
+	Type   int32  `json:"type,omitempty"`   // 图片：类型，1-原图、2-大图、3-缩略图
 	Url    string `json:"url,omitempty"`    // 图片：链接
 	Width  int32  `json:"width,omitempty"`  // 图片：宽
 	Height int32  `json:"height,omitempty"` // 图片：高
--- internal/logic/data/ent/format/video.go
+++ internal/logic/data/ent/format/video.go
@@ -4,10 +4,12 @@ type VideoContent struct {
 	VideoUrl    string `json:"video_url,omitempty"`    // 视频：链接
 	VideoSecond int32  `json:"video_second,omitempty"` // 视频：时长（秒）
 	VideoUuid   string `json:"video_uuid,omitempty"`   // 资源标识
+	VideoSize   int32  `json:"video_size"`             // 视频：大小
 	ThumbUrl    string `json:"thumb_url,omitempty"`    // 视频封面：链接
 	ThumbWidth  int32  `json:"thumb_width,omitempty"`  // 视频封面：宽
 	ThumbHeight int32  `json:"thumb_height,omitempty"` // 视频封面：高
 	ThumbUuid   string `json:"thumb_uuid,omitempty"`   // 资源标识
+	ThumbSize   int32  `json:"thumb_size"`             // 视频封面：大小
 }
 
 func (c VideoContent) GetType() MsgType {
