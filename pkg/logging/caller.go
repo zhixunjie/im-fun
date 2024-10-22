bf5a9ee9dbb27cd9f480b43fdada31ec100f6a53
JasonZhiZhi <JasonZhiZhi@poweronlabs.com>
2024-10-22 10:51:05 +0800

    log

--- pkg/logging/caller.go
+++ pkg/logging/caller.go
@@ -7,24 +7,6 @@ import (
 	"strings"
 )
 
-// printCaller 输出Caller信息
-func printCaller(f *runtime.Frame) (string, string) {
-	s := strings.Split(f.Function, ".")
-	funcName := s[len(s)-1]
-	dir, filename := path.Split(f.File)
-	baseDir := path.Base(dir)
-	return funcName, fmt.Sprintf("%v/%v:%v", baseDir, filename, f.Line)
-}
-
-// printCallerOther 输出Caller信息
-func printCallerOther(f *runtime.Frame) string {
-	s := strings.Split(f.Function, ".")
-	funcName := s[len(s)-1]
-	dir, filename := path.Split(f.File)
-	baseDir := path.Base(dir)
-	return fmt.Sprintf("%v/%v:%v func[%v]", baseDir, filename, f.Line, funcName)
-}
-
 // 针对im-fun项目的Caller获取
 func printCallerForImFun(f *runtime.Frame) string {
 	pc, file, line, _ := runtime.Caller(9)
