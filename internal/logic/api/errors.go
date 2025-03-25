3bfc9d9f42bb8b2e6577e9b1850985584ab27feb
JasonZhiZhi <JasonZhiZhi101@poweronlabs.ai>
2025-03-25 15:14:49 +0800

    feat(*): code整理

--- internal/logic/api/errors.go
+++ internal/logic/api/errors.go
@@ -7,5 +7,5 @@ import "errors"
 var (
 	ErrContactNotExists     = errors.New("contact is not exists")
 	ErrSenderTypeNotAllow   = errors.New("sender type not allow")
-	ErrReceiverTypeNotAllow = errors.New("sender type not allow")
+	ErrReceiverTypeNotAllow = errors.New("receiver type not allow")
 )
