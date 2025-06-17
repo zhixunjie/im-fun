4b5433eeea685d885f90577a39a1aab0f5e96558
JasonZhiZhi <JasonZhiZhi101@poweronlabs.ai>
2025-06-17 19:18:14 +0800

    feat(*): 修改日志颜色

--- pkg/logging/format.go
+++ pkg/logging/format.go
@@ -14,7 +14,6 @@ type myFormatter struct {
 }
 
 func (f *myFormatter) Format(entry *logrus.Entry) ([]byte, error) {
-
 	var b *bytes.Buffer
 	if entry.Buffer != nil {
 		b = entry.Buffer
@@ -23,6 +22,7 @@ func (f *myFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	}
 
 	// time
+	_, _ = fmt.Fprintf(b, "\x1b[%dm", colorDefault) // 添加颜色
 	time := entry.Time.Format("2006-01-02 15:04:05.000")
 	b.WriteString(time)
 
@@ -34,9 +34,11 @@ func (f *myFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	b.WriteString("\x1b[0m") // 去掉颜色
 
 	// caller
+	_, _ = fmt.Fprintf(b, "\x1b[%dm", colorYellow) // 添加颜色
 	b.WriteString(" " + printCallerForImFun(entry.Caller))
 
 	// msg
+	_, _ = fmt.Fprintf(b, "\x1b[%dm", colorDefault) // 添加颜色
 	b.WriteString(" ")
 	b.WriteString(entry.Message)
 
@@ -45,6 +47,7 @@ func (f *myFormatter) Format(entry *logrus.Entry) ([]byte, error) {
 	f.writeFields(b, entry)
 
 	b.WriteByte('\n')
+	b.WriteString("\x1b[0m") // 去掉颜色
 	return b.Bytes(), nil
 }
 
@@ -68,10 +71,11 @@ func (f *myFormatter) writeField(b *bytes.Buffer, entry *logrus.Entry, field str
 }
 
 const (
-	colorRed    = 31
-	colorYellow = 33
-	colorBlue   = 36
-	colorGray   = 37
+	colorRed     = 31
+	colorYellow  = 33
+	colorBlue    = 36
+	colorGray    = 37
+	colorDefault = 37
 )
 
 func getColorByLevel(level logrus.Level) int {
