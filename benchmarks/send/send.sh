9f52ee47c496b615523469b5baa342ef49111bca
JasonZhiZhi <JasonZhiZhi101@poweronlabs.ai>
2025-06-17 18:34:46 +0800

    feat(*): 优化 tcp session 管理

--- benchmarks/send/send.sh
+++ benchmarks/send/send.sh
@@ -1,6 +1,6 @@
 #for ((i=0;i<100000;i++)); do
 for ((i=0;i<10;i++)); do
-curl -XPOST  "http://127.0.0.1:8080/im/send/to/users" \
+curl -XPOST  "http://127.0.0.1:8080/im/send/to/users/by/ids" \
 -d '{
         "uni_ids": [
            "1"
