2281cf75d988c3f50511886a5a6491df478d2b3b
JasonZhiZhi <unknown@gmail.com>
2025-06-18 03:40:19 +0800

    feat: 优化client

--- internal/logic/data/ent/request/user.go
+++ internal/logic/data/ent/request/user.go
@@ -5,7 +5,7 @@ import "github.com/zhixunjie/im-fun/pkg/gmodel"
 type LoginReq struct {
 	gmodel.Atom
 	AccountType    gmodel.AccountType `json:"account_type" binding:"required,gte=1,lte=2"` // 账号类型
-	AccountID      string             `json:"account_id" binding:"required,gte=1"`         // 手机号码/设备号/三方平台账号
+	AccountID      string             `json:"account_id" binding:"required,gte=1"`         // 设备号/手机号码/三方平台账号
 	ThirdLoginInfo *ThirdLoginInfo    `json:"third_login_info,omitempty"`                  // 第三方平台登录信息
 }
 
