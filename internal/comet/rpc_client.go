8d9c6a78551c51dec68b1acf524144b64c7e14cf
JasonZhiZhi <JasonZhiZhi101@poweronlabs.ai>
2025-06-17 14:40:13 +0800

    feat(*): grpc connection

--- internal/comet/rpc_client.go
+++ internal/comet/rpc_client.go
@@ -5,15 +5,11 @@ import (
 	"github.com/zhixunjie/im-fun/api/pb"
 	"github.com/zhixunjie/im-fun/internal/comet/conf"
 	"github.com/zhixunjie/im-fun/pkg/grpc"
-	"time"
 )
 
 func newLogicClient(conf *conf.RPCClient) pb.LogicClient {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
-	defer cancel()
-
 	// dial
-	conn, err := grpc.Connection(ctx, "discovery:///logic", true)
+	conn, err := grpc.Connection(context.Background(), "discovery:///logic", true)
 	if err != nil {
 		panic(err)
 	}
